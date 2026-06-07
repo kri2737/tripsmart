9053d4ed295c9f4d981d72b6c57a340121ef8921
Kritika Singh <kritikasingh@Kritikas-MacBook-Air.local>
2026-06-07 23:36:35 +0545

    Connected MongoDB to server

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
 
+//middleware
+app.use(cors());
 app.use(express.json());
 
 app.get("/" , (req,res) =>{
       res.json({message : `Tripsmart API is running`});
 });
 
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("MongoDB Connected");
+    app.listen(process.env.PORT , () =>{
+        console.log(`Server running on port ${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("Connection failed", err);
+});
 app.listen(8000,()=>{
     console.log("server running on port 8000");
 });
