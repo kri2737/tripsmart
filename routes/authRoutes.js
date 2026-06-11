ef5d90f25c190b1c12254cb313398cbb4d28691f
Kritika Singh <kritikasingh@Kritikas-MacBook-Air.local>
2026-06-11 17:53:50 +0545

    Auth middleware complete - protected routes working

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const{register , login} = require("../controllers/authController");
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.post("/register", register);
 router.post("/login", login);
 
 module.exports= router;
+
+router.get('/profile', authMiddleware, (req, res) => {
+    res.json({ message: 'Protected route works', userId: req.userId });
+  });
