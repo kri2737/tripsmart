ff1b1d990d12d9b15c3c6ca0f98ee9eca70bdb80
kri2737 <kritikasingh6888@gmail.com>
2026-06-15 14:31:47 +0545

    Signup page built and connected to backend

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,3 +1,4 @@
+if (document.getElementById('loginForm')){
 document.getElementById('loginForm').addEventListener('submit', async function(e) {
     e.preventDefault();
 
@@ -26,3 +27,35 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
         document.getElementById('message').textContent = 'Something went wrong';
     }
 });
+}
+if (document.getElementById('signUpForm')) {
+    document.getElementById('signUpForm').addEventListener('submit', async function(e) {
+        e.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        try {
+            const response = await fetch('http://localhost:8000/api/auth/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ name, email, password })
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                document.getElementById('message').textContent = 'Registration successful!';
+                localStorage.setItem('token', data.token);
+            } else {
+                document.getElementById('message').textContent = data.message;
+            }
+
+        } catch (error) {
+            document.getElementById('message').textContent = 'Something went wrong';
+        }
+    });
+}
